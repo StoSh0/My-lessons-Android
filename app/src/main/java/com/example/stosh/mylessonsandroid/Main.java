8df4ed1b86743e501451698475b8b28d0d0b1c0e
Ruslan Stosyk <stosh.lv.ua@gmail.com>
2016-10-05 13:12:59 +0300

    Add ButterKnife and Complete task 2

--- app/src/main/java/com/example/stosh/mylessonsandroid/Main.java
+++ app/src/main/java/com/example/stosh/mylessonsandroid/Main.java
@@ -1,35 +1,36 @@
 package com.example.stosh.mylessonsandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class Main extends AppCompatActivity {
 
+    @BindView(R.id.editText)
     EditText editText;
-    Button buttonOk;
+    @BindView(R.id.textView)
     TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        editText = (EditText) findViewById(R.id.editText);
-        buttonOk = (Button) findViewById(R.id.buttonOk);
-        textView =  (TextView)findViewById(R.id.textView);
     }
 
-    public void clickButtonOk(View view){
+    @OnClick(R.id.buttonOk)
+    public void onButtonClick() {
         if (TextUtils.isEmpty(editText.getText().toString())) {
             textView.setText(R.string.edit_text_is_null);
             return;
         }
-
         textView.setText(editText.getText().toString());
     }
 }
