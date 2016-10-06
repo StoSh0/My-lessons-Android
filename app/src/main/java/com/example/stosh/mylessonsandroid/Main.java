fc268489cfef74ed9627a59d5d8ab1d97e07bc1f
Ruslan Stosyk <stosh.lv.ua@gmail.com>
2016-10-07 01:47:25 +0300

    I dont know what doing next, I create DataPiker and i dont know how delete notification when i press button_stop, fuck you android documentation

--- app/src/main/java/com/example/stosh/mylessonsandroid/Main.java
+++ app/src/main/java/com/example/stosh/mylessonsandroid/Main.java
@@ -1,7 +1,16 @@
 package com.example.stosh.mylessonsandroid;
 
+
+import android.app.DialogFragment;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -12,33 +21,58 @@ import butterknife.Unbinder;
 
 public class Main extends AppCompatActivity {
 
+    private Unbinder unbinder;
+    public static int year = 2016, month = 1, day = 1;
+    public static final int ID_NOTIFICATION = 20;
+
     @BindView(R.id.textView)
     TextView textView;
-    private Unbinder unbinder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         unbinder = ButterKnife.bind(this);
-
     }
 
-    @OnClick({R.id.button_call_time, R.id.button_start, R.id.button_stop})
+    @OnClick({/*R.id.button_call_time,*/ R.id.button_start, R.id.button_stop})
     public void onButtonClick(Button button) {
         switch (button.getId()) {
-            case R.id.button_call_time:
-                textView.setText("button_call_time_picker");
-                break;
+            /*case R.id.button_call_time:
+                showDatePickerDialog(button);
+                break;*/
             case R.id.button_start:
-                textView.setText("button_call_time_start");
-                break;
+                NotificationCompat.Builder mBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.start)
+                        .setContentTitle("Сповіщення")
+                        .setContentText("Рік " + year + " Місяць " + month + " День " + day)
+                        .setAutoCancel(true);
+
+                Intent resultIntent = new Intent(this, Main.class);
+
+
+                TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+
+                stackBuilder.addParentStack(Main.class);
+
+                stackBuilder.addNextIntent(resultIntent);
+                PendingIntent resultPendingIntent =
+                        stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+                mBuilder.setContentIntent(resultPendingIntent);
+                NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                mNotificationManager.notify(ID_NOTIFICATION, mBuilder.build());
             case R.id.button_stop:
-                textView.setText("button_call_time_stop");
+
                 break;
         }
-
     }
+
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getFragmentManager(), "datePicker");
+    }
+
+
 
     @Override
     protected void onDestroy() {
