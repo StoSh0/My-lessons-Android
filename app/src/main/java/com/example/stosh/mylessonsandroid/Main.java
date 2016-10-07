d062665f732c9f13cfde417f93adb3ce5cce8fec
Ruslan Stosyk <stosh.lv.ua@gmail.com>
2016-10-07 14:02:25 +0300

    complete Task 3

--- app/src/main/java/com/example/stosh/mylessonsandroid/Main.java
+++ app/src/main/java/com/example/stosh/mylessonsandroid/Main.java
@@ -1,7 +1,8 @@
 package com.example.stosh.mylessonsandroid;
 
 
-import android.app.DialogFragment;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
@@ -10,8 +11,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.NotificationCompat;
-import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -21,12 +22,19 @@ import butterknife.Unbinder;
 
 public class Main extends AppCompatActivity {
 
-    private Unbinder unbinder;
-    public static int year = 2016, month = 1, day = 1;
+    public static final int ID_DIALOG_DATE = 1;
     public static final int ID_NOTIFICATION = 20;
+
+    private Unbinder unbinder;
+
+    public NotificationManager mNotificationManager;
+    public NotificationCompat.Builder mBuilder;
+
+    public int my_year = 2016, my_month = 10, my_day = 7;
 
     @BindView(R.id.textView)
     TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,44 +43,66 @@ public class Main extends AppCompatActivity {
         unbinder = ButterKnife.bind(this);
     }
 
-    @OnClick({/*R.id.button_call_time,*/ R.id.button_start, R.id.button_stop})
+    @OnClick({R.id.button_call_time, R.id.button_start, R.id.button_stop})
     public void onButtonClick(Button button) {
         switch (button.getId()) {
-            /*case R.id.button_call_time:
-                showDatePickerDialog(button);
-                break;*/
+            case R.id.button_call_time:
+                showDialog(ID_DIALOG_DATE);
+                break;
             case R.id.button_start:
-                NotificationCompat.Builder mBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
-                        .setSmallIcon(R.drawable.start)
-                        .setContentTitle("Сповіщення")
-                        .setContentText("Рік " + year + " Місяць " + month + " День " + day)
-                        .setAutoCancel(true);
+                mBuilder = (NotificationCompat.Builder)
+                        new NotificationCompat.Builder(this)
+                                .setSmallIcon(R.drawable.start)
+                                .setContentTitle("Сповіщення")
+                                .setContentText(textView.getText())
+                                .setAutoCancel(true);
 
                 Intent resultIntent = new Intent(this, Main.class);
 
-
                 TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-
                 stackBuilder.addParentStack(Main.class);
-
                 stackBuilder.addNextIntent(resultIntent);
+
                 PendingIntent resultPendingIntent =
                         stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
                 mBuilder.setContentIntent(resultPendingIntent);
-                NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+                mNotificationManager = (NotificationManager)
+                        getSystemService(Context.NOTIFICATION_SERVICE);
                 mNotificationManager.notify(ID_NOTIFICATION, mBuilder.build());
+                break;
             case R.id.button_stop:
 
+                mNotificationManager.cancel(ID_NOTIFICATION);
                 break;
         }
     }
 
-    public void showDatePickerDialog(View v) {
-        DialogFragment newFragment = new DatePickerFragment();
-        newFragment.show(getFragmentManager(), "datePicker");
+    protected Dialog onCreateDialog(int id) {
+        if (id == ID_DIALOG_DATE) {
+            DatePickerDialog myDatePicker = new DatePickerDialog(
+                    this,
+                    myListenerDateDialog,
+                    my_year,
+                    my_month,
+                    my_day
+            );
+            return myDatePicker;
+        }
+        return super.onCreateDialog(id);
     }
 
-
+    private DatePickerDialog.OnDateSetListener myListenerDateDialog =
+            new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                    my_year = year;
+                    my_month = month;
+                    my_day = dayOfMonth;
+                    textView.setText("Вибрана дата:" + my_year + "." + my_month + "." + my_day);
+                }
+            };
 
     @Override
     protected void onDestroy() {
